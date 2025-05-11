4c7a1befb3f04a48d19fb92d71088e5054be8062
Bisao <godofglobal@gmail.com>
2025-05-11 16:28:16 +0000

    Restored to 'b97bca0d9c11750d6a82cf6dafe39b1906365624'
    Replit-Restored-To: b97bca0d9c11750d6a82cf6dafe39b1906365624

--- game/scenes/components/BaseNPC.js
+++ game/scenes/components/BaseNPC.js
@@ -191,6 +191,9 @@ export default class BaseNPC {
         const {tileX, tileY} = this.scene.grid.gridToIso(newX, newY);
         this.isMoving = true;
 
+        // Configura a animação padrão
+        this.sprite.setTexture('farmer1');
+
         // Mostra o sprite ao sair da casa
         this.sprite.setVisible(true);
 
