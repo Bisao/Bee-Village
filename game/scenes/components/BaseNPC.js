45f2e86196e0eaedb5834017c00739559d8431ec
Bisao <godofglobal@gmail.com>
2025-05-13 01:52:08 -0300

    Add files via upload

--- game/scenes/components/BaseNPC.js
+++ game/scenes/components/BaseNPC.js
@@ -5,6 +5,7 @@ export default class BaseNPC {
         this.gridX = x;
         this.gridY = y;
         this.isMoving = false;
+        this.isAutonomous = true;
         
         // Configurações customizáveis
         this.config = {
@@ -92,7 +93,23 @@ export default class BaseNPC {
         this.scene.showNPCControls(this);
     }
 
-    // Removed autonomous movement code
+    startAutonomousMovement() {
+        if (!this.isAutonomous) return;
+
+        if (this.currentJob === 'lumber') {
+            const lumberSystem = this.scene.lumberSystem;
+            if (lumberSystem && lumberSystem.isWorking) {
+                const tree = lumberSystem.findNearestTree(this);
+                if (tree) {
+                    lumberSystem.moveToTree(this, tree);
+                }
+            }
+            return;
+        }
+
+        // Se não está trabalhando, volta para casa
+        this.returnHome();
+    }
 
     setRestMode(enabled) {
         if (enabled) {
